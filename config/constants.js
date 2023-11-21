ac6bf2851bd80be17b529b709a29f3bd99e90973
Akash MAllela <akashlucky1414@gmail.com>
2023-11-21 13:18:58 +0530

    for render

--- config/constants.js
+++ config/constants.js
@@ -10,3 +10,4 @@ export const DEV_MODE = (process.env.NODE_ENV === "development");
 export const CLIENT_ID = 'http://590811174729-j0v5ub1ju0ajha6h0ahn0ltdrju4ttor.apps.googleusercontent.com';
 export const JWT_SECRET = process.env.JWT_SECRET || "secret";
 export const POST_MARK_SERVER_TOKEN = process.env.POST_MARK_SERVER_TOKEN;
+export const CLIENT_SECRET = process.env.CLIENT_SECRET
--- utils/GoogleOuth.js
+++ utils/GoogleOuth.js
@@ -1,8 +1,9 @@
 import { OAuth2Client } from 'google-auth-library';
+import { CLIENT_SECRET } from '../config/constants';
 
 export const CLIENT_ID = '590811174729-j0v5ub1ju0ajha6h0ahn0ltdrju4ttor.apps.googleusercontent.com'
 async function verify(token) {
-const client = new OAuth2Client(CLIENT_ID,'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');
+const client = new OAuth2Client(CLIENT_ID,CLIENT_SECRET);
   const ticket = await client.verifyIdToken({
       idToken: token,
       audience: CLIENT_ID,
