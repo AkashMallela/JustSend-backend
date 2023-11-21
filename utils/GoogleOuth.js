32ef650ac501cf7831a7165ea1f9c8995d4b4829
Akash MAllela <akashlucky1414@gmail.com>
2023-11-21 13:23:00 +0530

    for render

--- utils/GoogleOuth.js
+++ utils/GoogleOuth.js
@@ -1,5 +1,5 @@
 import { OAuth2Client } from 'google-auth-library';
-import { CLIENT_SECRET } from '../config/constants';
+import { CLIENT_SECRET } from '../config/constants.js';
 
 export const CLIENT_ID = '590811174729-j0v5ub1ju0ajha6h0ahn0ltdrju4ttor.apps.googleusercontent.com'
 async function verify(token) {
